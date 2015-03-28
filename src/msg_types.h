6adfb734f2e4bb8104aa7e8cef3f149e69dffd80
Mario Kostelac <mario.kostelac@gmail.com>
2015-03-28 08:51:22 +0000

    added methods for detecting wheter overlap uses read end or not

--- src/msg_types.h
+++ src/msg_types.h
@@ -1,8 +1,7 @@
-
 #ifndef _AMOS_MSG_TYPES_H
 #define _AMOS_MSG_TYPES_H
-
 #include <cstdint>
+#include <cassert>
 
 namespace AMOS {
 
@@ -41,9 +40,54 @@ namespace AMOS {
 
       Overlap(const uint32_t r1, const uint32_t r2, const char adj, const int32_t ahg, const int32_t bhg, const int32_t scr)
         :read1(r1), read2(r2), adjacency(adj), a_hang(ahg), b_hang(bhg), score(scr)
-      {}
+      {
+        assert(adj == 'N' || adj == 'I');
+      }
 
       Overlap() {}
+
+      //
+      // ahang - number of bases from read A that come before the overlap.
+      // bhang - number of bases from read B that come after the overlap.
+      //
+      // read a      -------------------|-------------->     bhang
+      // read b            ahang     ---------------|--------------->
+      //
+      // read a           -ahang     ---------------|--------------->
+      // read b      -------------------|-------------->     -bhang
+      //
+
+      bool use_start(const uint32_t read) const {
+        if (read != read1 && read != read2) {
+          return false;
+        }
+
+        if (read == read1 && a_hang <= 0) {
+          return true;
+        } else if (read == read2 && adjacency == 'N' && a_hang >= 0) {
+          return true;
+        } else if (read == read2 && adjacency == 'I' && b_hang <= 0) {
+          return true;
+        }
+
+        return false;
+      }
+
+      bool use_end(const uint32_t read) const {
+        if (read != read1 && read != read2) {
+          return false;
+        }
+
+        if (read == read1 && b_hang >= 0) {
+          return true;
+        } else if (read == read2 && adjacency == 'N' && b_hang <= 0) {
+          return true;
+        } else if (read == read2 && adjacency == 'I' && a_hang >= 0) {
+          return true;
+        }
+
+        return false;
+      }
   };
 }
 #endif
